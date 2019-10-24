dfc8052fb3bc2beb06ab41cef2bff45db56f6de7
u201816131 (La Rosa Toro, Gustavo) <u201816131@upc.edu.pe>
2019-10-23 23:07:49 -0500

    confirmación

---
+++ Lecura.h
@@ -0,0 +1,21 @@
+#ifndef LECTURA_H_
+#define LECTURA_H_
+
+#include <iostream>
+#include <iostream>
+#include <filesystem>
+#include <string>
+
+using namespace std;
+
+template <typename T>
+
+class Leer {
+	struct Node {
+
+	};
+public:
+	Leer() {}
+	~Leer() {}
+};
+#endif // !LECTURA_H_
